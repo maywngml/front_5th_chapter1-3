8ffd80279bd70129b729cc83eb9b9e70311193a7
서주희 <maywngml@naver.com>
2025-04-08 20:09:58 +0900

    feat: 깊은 비교 함수 기능 구현

--- src/@lib/equalities/deepEquals.ts
+++ src/@lib/equalities/deepEquals.ts
@@ -1,3 +1,24 @@
+import { isPrimitiveValue } from "@/utils";
+
 export function deepEquals<T>(objA: T, objB: T): boolean {
+  if (
+    isPrimitiveValue(objA) ||
+    isPrimitiveValue(objB) ||
+    objA === null ||
+    objB === null
+  ) {
+    return objA === objB;
+  }
+  if (typeof objA === "object" && typeof objB === "object") {
+    if (Array.isArray(objA) && Array.isArray(objB)) {
+      return objA.every((item, index) => deepEquals(item, objB[index]));
+    }
+    if (Object.keys(objA).length !== Object.keys(objB).length) {
+      return false;
+    }
+    return Object.keys(objA).every((key) =>
+      deepEquals(objA[key as keyof T], objB[key as keyof T]),
+    );
+  }
   return objA === objB;
 }
