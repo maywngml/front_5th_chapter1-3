6cf05ad140b9184e22d6e07a8587d72b023611cc
서주희 <maywngml@naver.com>
2025-04-08 19:32:18 +0900

    chore: 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default mergeConfig(
   defineConfig({
     plugins: [react()],
+    resolve: {
+      alias: [{ find: "@", replacement: "src" }],
+    },
   }),
   defineTestConfig({
     test: {
