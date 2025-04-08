380f3605b975150917c7317be45b111d7d0a20e9
서주희 <maywngml@naver.com>
2025-04-08 18:53:01 +0900

    feat: 얕은 비교 함수 구현

--- src/@lib/equalities/shallowEquals.ts
+++ src/@lib/equalities/shallowEquals.ts
@@ -1,3 +1,25 @@
 export function shallowEquals<T>(objA: T, objB: T): boolean {
-  return objA === objB;
+  if (objA === objB) {
+    return true;
+  }
+  if (
+    typeof objA !== "object" ||
+    objA === null ||
+    typeof objB !== "object" ||
+    objB === null
+  ) {
+    return false;
+  }
+
+  const keysA = Object.keys(objA);
+
+  if (keysA.length !== Object.keys(objB).length) {
+    return false;
+  }
+
+  return keysA.every(
+    (key) =>
+      Object.prototype.hasOwnProperty.call(objB, key) &&
+      objA[key as keyof T] === objB[key as keyof T],
+  );
 }
