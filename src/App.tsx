ae611bf0c74f886cb32f95ade40c9ca439a4f198
서주희 <maywngml@naver.com>
2025-04-10 04:29:26 +0900

    refactor: generateItems 함수가 최초 렌더시에만 호출되도록 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import React, { useState } from "react";
 import { generateItems } from "./utils";
 import {
@@ -13,7 +12,7 @@ import { UserProvider } from "./context/UserContext";
 
 // 메인 App 컴포넌트
 const App: React.FC = () => {
-  const [items, setItems] = useState(generateItems(1000));
+  const [items, setItems] = useState(() => generateItems(1000));
 
   const addItems = () => {
     setItems((prevItems) => [
@@ -30,10 +29,7 @@ const App: React.FC = () => {
             <div className="container mx-auto px-4 py-8">
               <div className="flex flex-col md:flex-row">
                 <div className="w-full md:w-1/2 md:pr-4">
-                  <ItemList
-                    items={items}
-                    onAddItemsClick={addItems}
-                  />
+                  <ItemList items={items} onAddItemsClick={addItems} />
                 </div>
                 <div className="w-full md:w-1/2 md:pl-4">
                   <ComplexForm />
