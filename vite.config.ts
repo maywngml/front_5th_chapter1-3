30f9a49a1bb201a43d44eb27f0b60e9b6105c7b1
서주희 <maywngml@naver.com>
2025-04-10 04:38:10 +0900

    chore: base 설정

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { resolve } from "path";
 
+const isCI = process.env.CI === "true";
+
 export default mergeConfig(
   defineConfig({
+    base: isCI ? "/" : "/front_5th_chapter1-3/",
     plugins: [react()],
     resolve: {
       alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
